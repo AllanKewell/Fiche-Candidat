191f72cc24f957f1d60c479611b2761e9afd4127
LamClefJob <lamtest@laposte.net>
2017-07-31 16:34:53 +0200

    update formulaire info + add testReponseForm

--- fiche-candidat-informatique.php
+++ fiche-candidat-informatique.php
@@ -5,52 +5,76 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Formulaire Logistique</title>
+	<title>Informatique</title>
 </head>
 <body>
 	<div id="wrapper" class="container">
 		<figure>
 			<img src="img/clefjob.png">
 		</figure>
-		<h1>Formulaire - Logistique</h1>
-		<form id="form">
+		<h1>Formulaire - Informatique</h1>
+		<form id="form" method="post" action="testReponseForm.php">
 			<fieldset>
 				<div class="form-group">
-					<div class="col-md-6">
-						<label class="control-label" for="name">Nom</label>
-						<input class="form-control" type="text" name="name" placeholder="Mon nom" />
+					<div class="col-md-2">
+						<label class="control-label">Civilité</label>
+						<select class="form-control custom-select">
+						  <option value="1">Mr.</option>
+						  <option value="2">Mme.</option>
+						</select>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-5">
+						<label class="control-label">Nom</label>
+						<input class="form-control" type="text" name="name" placeholder="Mon nom" required autofocus />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-5">
+						<label class="control-label">Prénom</label>
+						<input class="form-control" type="text" name="surname" placeholder="Mon prénom" required />
 					</div>
 				</div>
 				<div class="form-group">
 					<div class="col-md-6">
-						<label class="control-label" for="name">Prénom</label>
-						<input class="form-control" type="text" name="surname" placeholder="Mon prénom" />
+						<label class="control-label">Email</label>
+						<input class="form-control" type="email" name="city" placeholder="exemple@mail.com" required />
 					</div>
 				</div>
 				<div class="form-group">
 					<div class="col-md-6">
-						<label class="control-label" for="name">Adresse</label>
-						<input class="form-control" type="text" name="address" placeholder="N° de la rue + nom de la rue" />
+						<label class="control-label">Sexe</label>
+						<select class="form-control custom-select">
+						  <option value="1">Homme</option>
+						  <option value="2">Femme</option>
+						</select>
 					</div>
 				</div>
 				<div class="form-group">
 					<div class="col-md-6">
-						<label class="control-label" for="name">Email</label>
-						<input class="form-control" type="email" name="city" placeholder="Ma ville" />
+						<label class="control-label">Adresse</label>
+						<input class="form-control" type="text" name="address" placeholder="N° de la rue + nom de la rue" required />
 					</div>
 				</div>
 				<div class="form-group">
 					<div class="col-md-6">
-						<label class="control-label" for="text">Code Postal</label>
-						<input class="form-control" type="number" name="zipcode" placeholder="95140" />
+						<label class="control-label">Ville</label>
+						<input class="form-control" type="text" name="city" placeholder="Ville" required />
 					</div>
 				</div>
 				<div class="form-group">
 					<div class="col-md-6">
-						<label class="control-label" for="name">Téléphone</label>
+						<label class="control-label" text">Code Postal</label>
+						<input class="form-control" type="text" name="zipcode" placeholder="95140" required />
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-6">
+						<label class="control-label">Téléphone</label>
 						<div class="input-group">
 							<span class="input-group-addon">+33</span>
-							<input class="form-control" type="tel" name="phonenumber" placeholder="06 12 34 56 78" />
+							<input class="form-control" type="tel" name="phonenumber" placeholder="06 12 34 56 78" required />
 						</div>
 					</div>
 				</div>
